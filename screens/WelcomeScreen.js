cf242213ad13e0a30ff185bffc24136cc061aeb3
Foucsi <julien.foucartreact@gmail.com>
2022-12-31 10:11:31 +0100

    mise a jour add tweet

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TextInput } from "react-native";
 import React from "react";
 import { useSelector } from "react-redux";
 import Header from "../components/Header";
@@ -7,11 +7,21 @@ import { useState } from "react";
 import { useEffect } from "react";
 import fetchIp from "../fecthIp.json";
 import Tweet from "../components/Tweet";
+import { MaterialIcons } from "@expo/vector-icons";
+import { useDispatch } from "react-redux";
 
 export default function WelcomeScreen({ navigation }) {
   const [allTweets, setAllTweets] = useState([]);
+  const [postTweet, setPostTweet] = useState("");
+  const users = useSelector((state) => state.user.value);
+
+  const dispatch = useDispatch();
 
   useEffect(() => {
+    getRefresh();
+  }, []);
+
+  const getRefresh = () => {
     fetch(`http://${fetchIp.myIp}:3000/tweets/allTweets`)
       .then((res) => res.json())
       .then((data) => {
@@ -21,13 +31,52 @@ export default function WelcomeScreen({ navigation }) {
 
         // allTweets.map((e) => console.log(e[0].tweet));
       });
-  }, []);
+  };
+
+  const handlePostTweet = () => {
+    if (postTweet) {
+      fetch(`http://${fetchIp.myIp}:3000/tweets/addTweets/${users.username}`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ tweet: postTweet }),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.result) {
+            getRefresh();
+            dispatch(login({ tweet: postTweet }));
+          }
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+      setPostTweet("");
+    }
+  };
 
   return (
     <View style={styles.container}>
       <Header navigation={navigation} />
       <View style={{ height: "85%", width: "100%" }}>
-        <Text>Welcome All !</Text>
+        <View style={{ backgroundColor: "grey", height: "10%" }}>
+          <TextInput
+            value={postTweet}
+            onChangeText={(value) => setPostTweet(value)}
+            placeholder="add tweet"
+            placeholderTextColor="#fff"
+            style={{
+              paddingBottom: 10,
+              borderBottomColor: "#fff",
+              borderWidth: 1,
+            }}
+          />
+          <MaterialIcons
+            name="add-comment"
+            size={24}
+            color="#fff"
+            onPress={() => handlePostTweet()}
+          />
+        </View>
         <View style={{ width: "100%", height: "80%" }}>
           {allTweets.length > 0 ? (
             // Si la longueur de allTweets est supérieure à 0, on affiche les tweets
